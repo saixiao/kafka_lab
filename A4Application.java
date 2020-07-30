480e6d752af7c8e6bca89fdac87eee04c4f95e96
Kevin Wang <wh4wang@uwaterloo.ca>
2020-07-30 13:19:22 -0400

    a

--- A4Application.java
+++ A4Application.java
@@ -46,7 +46,7 @@ public class A4Application {
 
 	KTable<String, Long> studentLocations = studentLocationStreams
 			.mapValues(textLine -> Arrays.asList(textLine.toLowerCase().split(",")))
-			.map(textLine -> KeyValue.pair(textLine.get(0), textLine.get(1)), Materialized.<String, String, KeyValueStore<Bytes, byte[]>as("queryableStoreName"));
+			.map(textLine -> KeyValue.pair(textLine.get(0), textLine.get(1)), Materialized.<String, String, KeyValueStore<Bytes, byte[]>>as("queryableStoreName"));
 
 //		KTable<String, Long> wordCounts = studentLocations
 //				.flatMapValues(textLine-> Arrays.asList(textLine.toLowerCase().split(",")))
