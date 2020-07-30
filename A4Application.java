db0024a061387c129b6c9d2d680a4ae4529fe684
Kevin Wang <wh4wang@uwaterloo.ca>
2020-07-30 17:40:48 -0400

    a

--- A4Application.java
+++ A4Application.java
@@ -64,14 +64,9 @@ public class A4Application {
 				Serdes.String(), /* key */
 				Serdes.String())     /* value */
 		)
-		.reduce(
-			(aggValue, newValue) {
-				if(newValue != aggValue){
-					return newValue;
-				} else {
-					System.out.println("SAME VALUE");
-				}
-			}, /* adder */
+		.aggregate(
+			() -> 0L, /* initializer */
+			(aggKey, newValue, aggValue) -> newValue,
 			Materialized.<String, String, KeyValueStore<Bytes, byte[]>>as("student-location-store")
 		)
 		.groupBy(
