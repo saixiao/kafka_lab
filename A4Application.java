c0b118a4dec20e79e20ac8a12bdc7dbf0f622234
Kevin Wang <wh4wang@uwaterloo.ca>
2020-07-30 15:43:01 -0400

    a

--- A4Application.java
+++ A4Application.java
@@ -53,12 +53,13 @@ public class A4Application {
 		);
 
 	KTable<String, Long> currentClassCapacity = studentLocations
-		.groupBy(
-			(studentName, roomNumber) -> roomNumber,
-			Serialized.with(
-				Serdes.String(), /* key (note: type was modified) */
-				Serdes.String()) /* value (note: type was modified) */
-		)
+//		.groupBy(
+//			(studentName, roomNumber) -> roomNumber,
+//			Serialized.with(
+//				Serdes.String(), /* key (note: type was modified) */
+//				Serdes.String()) /* value (note: type was modified) */
+//		)
+		.groupBy((studentName, roomNumber) -> roomNumber)
 		.count(
 			Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("current-class-capacity")
 		);
